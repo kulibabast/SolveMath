bcb33c36c51c992ed22cae512940077c2ab820fb
kulibabast <110538089+kulibabast@users.noreply.github.com>
2024-03-22 20:00:05 +0300

    ADD ML Service

---
+++ Site/Layout/MlService/main.py
@@ -0,0 +1,20 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+model = GPT2LMHeadModel.from_pretrained(r"C:\Users\kulib\PycharmProjects\SolveMath\Site\Layout\MlService\finetuned-rugpt")
+tokenizer = GPT2Tokenizer.from_pretrained("sberbank-ai/rugpt3small_based_on_gpt2")
+
+
+def generate_response(input_text, max_length=100, temperature=0.7):
+    input_ids = tokenizer.encode(input_text, return_tensors="pt").to('cpu')
+    output = model.generate(
+        input_ids,
+        max_length=max_length,
+        temperature=temperature,
+        pad_token_id=tokenizer.eos_token_id,
+        num_return_sequences=1,
+        no_repeat_ngram_size=2,
+        top_k=5,
+        top_p=0.95,
+        eos_token_id=tokenizer.eos_token_id,
+        early_stopping=True
+    )
+    return tokenizer.decode(output[0], skip_special_tokens=True)
